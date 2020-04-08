4ff1dd149df994f5b7db7d7adafce18f0f174f71
Javier Naranjo <grinfuses@gmail.com>
2020-04-09 00:19:32 +0200

    Fix connections problems

--- controlHorario/www/js/api/get_registro.js
+++ controlHorario/www/js/api/get_registro.js
@@ -1,30 +1,90 @@
-
 $( "#buscar_registros" ).submit(function( event ) {
-    // get all the inputs into an array.
-    var $inputs = $('#buscar_registros :input');
-
-    // not sure if you wanted this, but I thought I'd add it.
-    // get an associative array of just the values.
-    var data = {};
-    $inputs.each(function() {
-        data[this.id] = $(this).val();
-    });
-    var url ="http://ec2-35-180-234-37.eu-west-3.compute.amazonaws.com:3000/buscarPorFechaAcumulando/" +data.dia_inicio+"/"+data.dia_fin;
-    var settings = {
-        "url": url,
-        "method": "POST",
-        "headers": {
-          "Content-Type": ["application/x-www-form-urlencoded"]
-        },
-        };
+  event.preventDefault()
+  var $inputs = $('#buscar_registros :input');
+  var data = {};
+  $inputs.each(function() {
+      data[this.id] = $(this).val();
+  });
+    var url_get ="http://ec2-35-180-234-37.eu-west-3.compute.amazonaws.com:3000/buscarPorFechaAcumulando/" +data.dia_inicio+"/"+data.dia_fin;
+    // $.ajax({
+    //       url: url_get,
+    //       type: 'post',
+    //       dataType: 'json',
+    //       error: function(XMLHttpRequest, textStatus, errorThrown) {
+    //         alert('Error al consultar el registro');
+    //         console.log(JSON.stringify(XMLHttpRequest));
+    //         console.log(JSON.stringify(textStatus));
+    //         console.log(JSON.stringify(errorThrown));
+    //       },
+    //       success: function (data) {
+    //         console.log("entra sucess");
+    //         console.log(data);
+    //       },
+    // timeout: 5000 // sets timeout to 3 seconds      
+    // });
+    // var settings = {
+    //   "url": "http://ec2-35-180-234-37.eu-west-3.compute.amazonaws.com:3000/buscarPorFechaAcumulando/2020-01-01/2020-08-01",
+    //   "method": "POST",
+    //   "timeout": 4000,
+    //   "headers": {
+    //     "Content-Type": "application/x-www-form-urlencoded"
+    //   },
+    // };
     
-        $.post(url, {}, function(result){
-            //$("span").html(result);
-            console.log(result);
-          });
-    // $.post(settings).done(function(data) {
-    //     console.log(data);
-    //   }).fail(function(data){
-    //     console.log("Try again champ!");
-    //   });
+    // $.ajax(settings).done(function (response) {
+    //   console.log(response);
+    // });
+    $.ajax({
+      url: "http://ec2-35-180-234-37.eu-west-3.compute.amazonaws.com:3000/buscarPorFechaAcumulando/2020-01-01/2020-08-01",
+      type: 'post',
+      timeout: 5000, 
+      dataType: 'json',
+      error: function(XMLHttpRequest, textStatus, errorThrown) {
+        alert('Error al crear el registro, reinténtelo más tarde');
+        console.log(JSON.stringify(XMLHttpRequest));
+        console.log(JSON.stringify(textStatus));
+        console.log(JSON.stringify(errorThrown));
+      },
+      success: function (data) {
+       alert('Registro creado.');
+       console.log(data);
+       var col = [];
+       for (var i = 0; i < data.length; i++) {
+           for (var key in data[i]) {
+               if (col.indexOf(key) === -1) {
+                   col.push(key);
+               }
+           }
+       }
+               // CREATE DYNAMIC TABLE.
+               var table = document.createElement("table");
+
+               // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+       
+               var tr = table.insertRow(-1);                   // TABLE ROW.
+
+               for (var i = 0; i < col.length; i++) {
+                   var th = document.createElement("th");      // TABLE HEADER.
+                   th.innerHTML = col[i];
+                   tr.appendChild(th);
+               }
+       
+               // ADD JSON DATA TO THE TABLE AS ROWS.
+               for (var i = 0; i < data.length; i++) {
+       
+                   tr = table.insertRow(-1);
+       
+                   for (var j = 0; j < col.length; j++) {
+                       var tabCell = tr.insertCell(-1);
+                       tabCell.innerHTML = data[i][col[j]];
+                   }
+               }
+       
+               // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+               var divContainer = document.getElementById("tabla_registros");
+               divContainer.innerHTML = "";
+               divContainer.appendChild(table);
+      }
+});
+    
   });
--- controlHorario/www/js/api/nuevo_registro.js
+++ controlHorario/www/js/api/nuevo_registro.js
@@ -1,25 +1,33 @@
 
 $( "#nuevo_registro" ).submit(function( event ) {
-    // get all the inputs into an array.
+    event.preventDefault()
     var $inputs = $('#nuevo_registro :input');
-
-    // not sure if you wanted this, but I thought I'd add it.
-    // get an associative array of just the values.
     var data = {};
     $inputs.each(function() {
+        if(this.id =="horaEntrada"){
+          var valor = $(this).val() + ":00";
+          data[this.id] = valor;
+        }else if(this.id =="horaSalida"){
+          var valor = $(this).val() + ":00";
+          data[this.id] = valor;
+        }else{
         data[this.id] = $(this).val();
+      }
     });
-    console.log(data);
-    var settings = {
-        "url": "http://ec2-35-180-234-37.eu-west-3.compute.amazonaws.com:3000/nuevoregistro/",
-        "method": "POST",
-        "headers": {
-          "Content-Type": ["application/x-www-form-urlencoded"]
-        },
-        "data": data,
-        };
-      
-      $.ajax(settings).done(function (response) {
-        console.log(response); 
-      });
+        $.ajax({
+          url: "http://ec2-35-180-234-37.eu-west-3.compute.amazonaws.com:3000/nuevoregistro/",
+          type: 'post',
+          data: data,
+          dataType: 'JSON',
+          error: function(XMLHttpRequest, textStatus, errorThrown) {
+            alert('Error al crear el registro, reinténtelo más tarde');
+            console.log(JSON.stringify(XMLHttpRequest));
+            console.log(JSON.stringify(textStatus));
+            console.log(JSON.stringify(errorThrown));
+          },
+          success: function (data) {
+           alert('Registro creado.');
+           $.mobile.changePage("index.html", "slideup");
+          }
+    });
   });
